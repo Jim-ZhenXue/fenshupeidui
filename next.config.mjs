07979476bfc524beee10e3c365ce55474e249a97
xz <xuezhen00@163.com>
2025-02-14 19:50:35 +0800

    a

--- next.config.mjs
+++ next.config.mjs
@@ -17,9 +17,40 @@ const nextConfig = {
     unoptimized: true,
   },
   experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
+    webpackBuildWorker: false,
+    parallelServerBuildTraces: false,
+    parallelServerCompiles: false,
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      // 生产环境客户端优化
+      config.optimization = {
+        ...config.optimization,
+        splitChunks: {
+          chunks: 'all',
+          minSize: 20000,
+          maxSize: 24000000, // 24MB 以确保在 25MB 限制之内
+          cacheGroups: {
+            default: false,
+            vendors: false,
+            commons: {
+              name: 'commons',
+              chunks: 'all',
+              minChunks: 2,
+            },
+            lib: {
+              test: /[\\/]node_modules[\\/]/,
+              name(module) {
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                return `npm.${packageName.replace('@', '')}`;
+              },
+              chunks: 'all',
+            },
+          },
+        },
+      };
+    }
+    return config;
   },
 }
 
